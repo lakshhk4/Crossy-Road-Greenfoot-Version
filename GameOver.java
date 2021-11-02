4a9fe7815648452e5133fe7e161d170e9767455c
lakshhk4 <53524072+lakshhk4@users.noreply.github.com>
2021-11-02 18:04:52 +0800

    Create GameOver.java

---
+++ GameOver.java
@@ -0,0 +1,9 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * One object of this class will be created once the game finishes. The object
+ * has an image of the 'Game Over Screen'
+ */
+public class GameOver extends Actor {
+
+}
